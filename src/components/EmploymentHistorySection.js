9eb04df709c316ab53a4213d9b9d63fd8a192934
Elijah Chou <ecool7079@gmail.com>
2025-03-11 21:56:41 -0400

    update employment history

--- src/components/EmploymentHistorySection.js
+++ src/components/EmploymentHistorySection.js
@@ -6,32 +6,57 @@ const employmentHistory = [
     {
         company: "JPMorganChase",
         role: "Software Engineer I",
-        duration: "September 2023 - May 2024",
-        description: ""
+        duration: "September 2023 - Present",
+        description:
+`- Engineered a multi-module Spring Boot application to simulate downstream system event consumption from start to deployment.
+- Composed API acceptance tests with internal automated testing framework to speed up regression testing by over 80%.
+- Leading scrum team's data modeling efforts to modernize 200+ legacy data elements for over 70 million debit cards.`
     },
     {
         company: "Georgia Tech Research Institute",
         role: "Student Software Engineer",
         duration: "September 2023 - May 2024",
-        description: ""
+        description:
+`- Conducted aircraft maintenance scheduling optimization research by leveraging Google OR-Tools.
+- Developed unit tests with Pytest and Hypothesis frameworks to validate the outputs of the proposed maintenance scheduling with established baselines.
+- Managed PostgreSQL database interfacing of schemas shared across collaborating GTRI laboratories.`
     },
+    {
+      company: "JPMorganChase",
+      role: "Software Engineer Academic Intern",
+      duration: "September 2023 - December 2023",
+      description:
+`- Created innovative metric counters using Micrometer for a Spring Boot application, visualizing data on a Grafana dashboard.
+- Updated Prometheus instances in development and QA testing environments to scrape new metrics.
+- Migrated key card information to an Oracle database using Liquibase, Oracle SQL Developer, and SQL scripts.`
+  },
     {
         company: "JPMorganChase",
         role: "Software Engineer Intern",
-        duration: "June 2023 - December 2023",
-        description: "Worked on Kubernetes, Kafka, Jenkins, and Airflow. Contributed to a project that can save the company $5 million per year."
+        duration: "June 2023 - August 2023",
+        description:
+`- Implemented a cost-saving framework for the Chase Debit Platform's backend, reducing annual losses by $5 million.
+- Orchestrated workflows with private cloud integration and configured inter-application network policies using Python.
+- Utilized Spring Integration to connect the backend application with existing Kafka topics and on-prem APIs.
+- Won 1st Place in the JPMC Global Innovation Week Hackathon in the Atlanta Tech Center for brainstorming and developing a new, streamlined onboarding tool for incoming hires.`
     },
     {
         company: "Georgia Tech Research Institute",
-        role: "Software Engineer Student Assistant",
+        role: "Software Engineer Intern",
         duration: "January 2023 - April 2023",
-        description: "Developed a Python Flask web application for naval base fuel ordering system. Worked with Docker and MVC framework."
+        description:
+`- Revamped a Python Flask web application, updating over 30 user features within an MVC architecture framework.
+- Developed 8 new user screens with HTML/CSS and SQL scripts, updating information in the connected PostgreSQL server.
+- Automated web application security testing using GitLab CI/CD YML files with GitLab-provided runners.`
     },
     {
         company: "AT&T",
         role: "Software Developer Intern",
         duration: "June 2022 - August 2022",
-        description: "Developed an immutable ledger of AI and ML models using Corda blockchain framework. Created a front-end UI with Springboot and AngularJS."
+        description:
+`- Conducted cutting-edge research with AT&T CDO inventive scientists to create a blockchain-based AI transparency proof-of-concept.
+- Prototyped a VR social media live streaming platform in an Agile scrum team of 5 interns for the annual Intern Innovation Challenge.
+- Presented my work to the AVP of Inventive Science at the Chief Data Office monthly meeting and at the Intern Expo.`
     },
   // Add more employment history as needed
 ];
